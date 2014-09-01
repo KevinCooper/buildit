1f65e8b68624c455b742d8269839e084ef9e6a26
Kevin Cooper <c15kevin.cooper@usafa.edu>
2014-09-01 15:55:55 -0600

    Big rewrite and now everything works with the AEs and MD5 routines.  Is able to successfully able to determine file integrity compromises even when a valid token is presented for addition of new information.

--- src/logappend.c
+++ src/logappend.c
@@ -15,10 +15,28 @@
 
 void batch(logappend_args args);
 void processLine(logappend_args args, int32_t CheckAndHashBool);
-void printMD5(char * toPrint);
+void checkMahFile(logappend_args args);
+
+MD5_CTX oldMD5;
+MD5_CTX currentMD5;
+MD5_CTX newMD5;
+
+unsigned char oldMD5String[MD5_DIGEST_LENGTH + 1];
+unsigned char currentMD5String[MD5_DIGEST_LENGTH + 1];
+unsigned char newMD5String[MD5_DIGEST_LENGTH + 1];
+
+uint32_t oldHashExists;
+uint32_t firstRun;
+
+HT* allMahHashes = NULL;
 
 int main(int argc, char * argv[]) {
+	if (argc < 3)
+		invalid();
+	oldHashExists = 1;
+	firstRun = 1;
 	logappend_args args = opt_parser(argc, argv);
+	allMahHashes = ht_create(65536);
 
 	if (args.returnStatus == -1)
 		invalid();
@@ -26,15 +44,17 @@ int main(int argc, char * argv[]) {
 	if (args.batchFile) {
 		batch(args);
 	} else {
+		checkMahFile(args);
 		processLine(args, 1);
 	}
 }
 
 void batch(logappend_args args) {
-	FILE* batchFile;
-	int32_t fileSize;
+	FILE* batchFile = NULL;
+	int32_t fileSize = 0;
 	size_t bytes = 0;
-	ssize_t read;
+	ssize_t read = 0;
+	int32_t firstRun = 1;
 	char * line = NULL;
 	char interString[256];
 
@@ -45,15 +65,19 @@ void batch(logappend_args args) {
 
 	while ((read = getline(&line, &bytes, batchFile)) != -1 && fileSize > 16) {
 
-		int len = strlen(	line);
+		int len = strlen(line);
 		fileSize = fileSize - len;
 		// RERUN COMMANDS CAUZE LOGIC!
 		sprintf(interString, "./logappend %s", line);
 		int tempc;
 		char ** tempv = argv_split(interString, &tempc);
 		logappend_args temp = opt_parser(tempc, tempv);
-		if (temp.batchFile != NULL)
+		if (temp.batchFile)
 			invalid();
+		if (firstRun) {
+			checkMahFile(temp);
+			firstRun = 0;
+		}
 		processLine(temp, fileSize < 10 ? 1 : 0);
 		bzero(interString, 256);
 		argv_free(tempv);
@@ -62,132 +86,98 @@ void batch(logappend_args args) {
 
 }
 
-void processLine(logappend_args args, int32_t CheckAndHashBool) {
+void processLine(logappend_args args, int32_t isLastLine) {
+	unsigned char * newMD5_S;
+	MD5_Update(&newMD5, args.toString, strlen(args.toString));
+	int32_t fileSize = 0;
+	FILE* mahFile = NULL;
+	unsigned int md5len = MD5_DIGEST_LENGTH;
+	//Make sure it is a good new first line
+	if (check_logic(&args) == -1)
+		invalid();
 
-	allMahHashes = ht_create(65536);
-	/* "opaque" encryption, decryption ctx structures that libcrypto uses to record
-	 status of enc/dec operations */
-	EVP_CIPHER_CTX en, de;
-	unsigned int salt[] = { 12345, 54321 };
-	unsigned char *key_data;
-	int key_data_len;
-	char interString[256];
+	fileSize = fsize(args.logName);
+	if(fileSize < 16){
+		mahFile = fopen(args.logName, "w+");
+	}else{
+		mahFile = fopen(args.logName, "r+");
+	}
+	//Write the current line to the file
+	if (fileSize < 16) {
+		fwrite(args.toString, sizeof(char), strlen(args.toString), mahFile);
+	} else {
+		if (firstRun) {
+			fseek(mahFile, -1 * MD5_DIGEST_LENGTH, SEEK_END);
+			firstRun = 0;
+		} else {
+			fseek(mahFile, 0, SEEK_END);
+		}
+		fwrite(args.toString, sizeof(char), strlen(args.toString), mahFile);
+	}
+	if (isLastLine) {
+		MD5_Final(newMD5_S, &newMD5);
+		EVP_CIPHER_CTX en, de;
+		unsigned int salt[] = { 12345, 54321 };
+		if (aes_init(args.token, strlen(args.token), (unsigned char *) &salt, &en, &de)) {
+			printf("Couldn't initialize AES cipher\n");
+			invalid();
+		}
+		char * ciphertext = aes_encrypt(&en, newMD5_S, &md5len);
+		fwrite(ciphertext, sizeof(char), MD5_DIGEST_LENGTH, mahFile);
+	}
+	fclose(mahFile);
+}
 
-	int32_t newFile = 0;
+void checkMahFile(logappend_args args) {
+	int32_t fileSize = 0;
+	FILE* mahFile = NULL;
+	char * pathname = args.logName;
 
+	unsigned int md5len = MD5_DIGEST_LENGTH;
+	char* oldMD5 = calloc(MD5_DIGEST_LENGTH + 1, 1);
+	MD5_Init(&currentMD5);
+	MD5_Init(&newMD5);
 	char * line = NULL;
 	size_t bytes = 0;
 	ssize_t read;
-	MD5_CTX mdContext;
-	MD5_CTX mdContextWithUserInput;
-	int i = 0;
-	int32_t fileSize;
-	FILE* mahFile;
-	int NINJA = MD5_DIGEST_LENGTH;
-	int * len = &NINJA;
+	int status = 0;
 
-	unsigned char currentFileMD5[MD5_DIGEST_LENGTH + 1];
-	unsigned char encryptedFileMD5[MD5_DIGEST_LENGTH + 1];
-	unsigned char fileWithInputMD5[MD5_DIGEST_LENGTH + 1];
-	char * encryptedFileWithInputMD5 = calloc(MD5_DIGEST_LENGTH + 1, 0);
-	char * decryptedFileMD5 = calloc(MD5_DIGEST_LENGTH + 1, 0);
+	fileSize = fsize(pathname);
+	if (fileSize > 16) {
+		mahFile = fopen(pathname, "r+");
+	} else {
+		return;
+	}
+	//Create an MD5 of the file and verify the syntax stuff
+	while ((read = getline(&line, &bytes, mahFile)) != -1 && fileSize > 16) {
+		int len = strlen(line);
+		if (check_logic(&args) == -1)
+			invalid();
+		fileSize = fileSize - len;
+		MD5_Update(&currentMD5, line, len);
+		MD5_Update(&newMD5, line, len);
+	}
+	char *currentMD5_S = NULL;
+	MD5_Final(currentMD5_S, &currentMD5);
 
+	//Read encrypted MD5 from the file
+	status = fseek(mahFile, -1 * MD5_DIGEST_LENGTH, SEEK_END);
+	status = fread(oldMD5, sizeof(unsigned char), MD5_DIGEST_LENGTH, mahFile);
+
+	//Decrypt the old MD5
+	EVP_CIPHER_CTX en, de;
+	unsigned int salt[] = { 12345, 54321 };
 	if (aes_init(args.token, strlen(args.token), (unsigned char *) &salt, &en, &de)) {
 		printf("Couldn't initialize AES cipher\n");
 		invalid();
 	}
+	char* oldMD5_de_S = (char *) aes_decrypt(&de, oldMD5, &md5len);
 
-	//Open up the file or create it if it doesn't exist
-	fileSize = fsize(args.logName);
-	if (fileSize < 16) {
-		newFile = 1;
-		mahFile = fopen(args.logName, "w+");
-	} else {
-		mahFile = fopen(args.logName, "r+");
-	}
-	if (mahFile == NULL)
+	if (!memcmp(oldMD5_de_S, currentMD5_S, MD5_DIGEST_LENGTH)) {
 		invalid();
-
-	//Compute the MD5 of the file
-	MD5_Init(&mdContext);
-	MD5_Init(&mdContextWithUserInput);
-	if (!newFile && CheckAndHashBool) {
-		while ((read = getline(&line, &bytes, mahFile)) != -1 && fileSize > 16) {
-
-			int len = strlen(line);
-			fileSize = fileSize - len;
-
-			// RERUN COMMANDS CAUZE LOGIC!
-			sprintf(interString, "./logappend %s", line);
-			int tempc;
-			char ** tempv = argv_split(interString, &tempc);
-			logappend_args temp = opt_parser(tempc, tempv);
-			bzero(interString, 256);
-			argv_free(tempv);
-			if (check_logic(&temp) == -1)
-				invalid();
-			// FINISH LOGICZ
-
-			MD5_Update(&mdContext, line, len);
-			MD5_Update(&mdContextWithUserInput, line, len);
-		}
 	}
-	if (CheckAndHashBool && check_logic(&args) == -1)
-		invalid();
-	MD5_Update(&mdContextWithUserInput, args.toString, strlen(args.toString));
-	MD5_Final(currentFileMD5, &mdContext);
-	MD5_Final(fileWithInputMD5, &mdContextWithUserInput);
 
-	//Mah debug printf's
-	printf("%s", "MD5 of the current file:    ");
-	printMD5(currentFileMD5);
-	printf("%s", "MD5 of the updated file:    ");
-	printMD5(fileWithInputMD5);
-
-	//Check the old md5
-	int status;
-	if (!newFile) {
-		//Encrypted MD5 should be last 16 bytes of the file
-		status = fseek(mahFile, -1 * MD5_DIGEST_LENGTH, SEEK_END);
-		status = fread(encryptedFileMD5, sizeof(unsigned char), MD5_DIGEST_LENGTH, mahFile);
-		printf("The encrypted MD5 was:      ");
-		printMD5(encryptedFileMD5);
-		decryptedFileMD5 = (char *) aes_decrypt(&de, encryptedFileMD5, len);
-		decryptedFileMD5[MD5_DIGEST_LENGTH] = '\0';
-		printf("%s", "Decrypted current file MD5: ");
-		printMD5(decryptedFileMD5);
-
-		/*// DISABLE CRYPTO CHECK UNTIL WORKING
-		 for (i = 0; i < MD5_DIGEST_LENGTH; i++) {
-		 if ((unsigned char) decryptedFileMD5[i] != (unsigned char) currentFileMD5[i]) {
-		 fclose(mahFile);
-		 return -1;
-		 }
-		 }*/
-
-		//Go back to get ready to write over the old key with our new line of input
-		status = fseek(mahFile, -1 * MD5_DIGEST_LENGTH, SEEK_END);
-	}
-	status = fwrite(args.toString, 1, strlen(args.toString), mahFile);
-
-	//Write encrypted MD5
-	encryptedFileWithInputMD5 = aes_encrypt(&en, fileWithInputMD5, len);
-	status = fwrite(encryptedFileWithInputMD5, 1, MD5_DIGEST_LENGTH, mahFile);
-	printf("Wrote the encrypted MD5:    ");
-	printMD5(encryptedFileWithInputMD5);
-
-	//Cleanup
 	fclose(mahFile);
-	fflush(mahFile);
-	EVP_CIPHER_CTX_cleanup(&en);
-	EVP_CIPHER_CTX_cleanup(&de);
-}
-
-void printMD5(char * toPrint) {
-	int i;
-	printf("0x");
-	for (i = 0; i < MD5_DIGEST_LENGTH; i++)
-		printf("%02x", (unsigned char) toPrint[i]);
-	printf("%s", "\n");
+	return;
 }
 
